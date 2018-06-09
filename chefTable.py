2ce87f07976c9e61482bda948da1dee6987c621f
Jesus Vita <jesusvitaa@gmail.com>
2018-06-08 21:20:54 -0400

    Added a window to input information

--- chefTable.py
+++ chefTable.py
@@ -1,40 +1,110 @@
 import time
+import random
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
-# declaring the chrome driver
-chromedriver = r"C:\Users\Jesus\Desktop\python\driver\chromedriver.exe"
+# Functions
+def chef_table_fill_out():
+    # take in all the data inputted by the user and apply it to the website
+    # for this functions I will have it take in a list
 
-# creating a chrome driver
-driver = webdriver.Chrome(chromedriver)
+    # declaring the chrome driver
+    chromedriver = r"C:\Users\Jesus\Desktop\python\driver\chromedriver.exe"
+    driver = webdriver.Chrome(chromedriver)
 
-# timeout if not loaded
-driver.set_page_load_timeout(30)
+    # timeout if not loaded
+    driver.set_page_load_timeout(30)
 
-# website I want to get
-driver.get("http://profile.benihana.com/registration/")
+    # website to input the information too
+    driver.get("http://profile.benihana.com/registration/")
+
+    # setting a wait time before inputting to make sure everything is loaded
+    driver.implicitly_wait(30)
+
+
+    f_name = driver.find_element_by_id('First_Nm').send_keys(str(f_name_entry.get()))
+    l_name = driver.find_element_by_id('Last_Nm').send_keys(str(l_name_entry.get()))
+    email_address = driver.find_element_by_id('Email_Address').send_keys(str(email_entry.get()))
+    email_address_confirm = driver.find_element_by_id('Cnfm_Email_Address').send_keys(str(email_entry.get()))
+    address_line_1 = driver.find_element_by_id('AddressLine1').send_keys(str(address_entry.get()))
+    address_line_2 = driver.find_element_by_id('AddressLine2').send_keys('')
+    city = driver.find_element_by_id('City').send_keys(str(city_entry.get()))
+    state = Select(driver.find_element_by_id('State')).select_by_value(str(state_entry.get()))
+    zip_code = driver.find_element_by_id('Zip').send_keys(str(zip_code_entry.get()))
+
+    birthdate_month = Select(driver.find_element_by_id('bdaymonth')).select_by_value("07")
+    birthdate_day = Select(driver.find_element_by_id('bdayday')).select_by_value("7")
+    birthdate_year = Select(driver.find_element_by_id('bdayyear')).select_by_value("1990")
+    favorite_location = Select(driver.find_element_by_id('store_code')).select_by_value("1053")
+    phone_number_1 = driver.find_element_by_id('phonenum-1').send_keys('954')
+    phone_number_2 = driver.find_element_by_id('phonenum-2').send_keys('663')
+    phone_number_3 = driver.find_element_by_id('phonenum-3').send_keys('2294')
+
+
+def display():
+
+    # labels
+    f_name_label = tk.Label(text = "First Name: ").grid(column=0, row=0)
+    l_name_label = tk.Label(text = "Last Name: ").grid(column=0, row=1)
+    email_label = tk.Label(text = "Email Address: ").grid(column=0, row=2)
+    address_label = tk.Label(text = "Address: ").grid(column=0, row=3)
+    city_label = tk.Label(text = "City: ").grid(column=0, row=4)
+    state_label = tk.Label(text = "State: ").grid(column=0, row=5)
+    zip_code_label = tk.Label(text = "Zip Code: ").grid(column=0, row=6)
+    birthdate_label = tk.Label(text = "Birthdate: (mm/dd/yyyy)").grid(column=0, row=7)
+    favorite_location_label = tk.Label(text = "Favorite Location: ").grid(column=0, row=8)
+    phone_number_label = tk.Label(text = "Phone Number: (###-###-####)").grid(column=0, row=9)
+
+
+# create an organize list that will keep all information the user will input to be used to write to the website
+user_input = []
+States = [
+"AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA","MD","ME","MH","MI"
+,"MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA"
+,"VI","VT","WA","WI","WV","WY"
+]
+
+
+# GUI
+window = tk.Tk()
+window.title("Chef's Table Filler")
+window.geometry("400x400")
+
+display()
+f_name_entry = tk.Entry()
+l_name_entry = tk.Entry()
+email_entry = tk.Entry()
+address_entry = tk.Entry()
+city_entry = tk.Entry()
+state_entry = ttk.Combobox(window)
+state_entry["values"] = States
+state_entry.current(9)
+zip_code_entry = tk.Entry()
+birthdate_entry = tk.Entry()
+favorite_location_entry = tk.Entry()
+phone_number_entry = tk.Entry()
+f_name_entry.grid(column=1, row=0)
+l_name_entry.grid(column=1, row=1)
+email_entry.grid(column=1, row=2)
+address_entry.grid(column=1, row=3)
+city_entry.grid(column=1, row=4)
+state_entry.grid(column=1, row=5)
+zip_code_entry.grid(column=1, row=6)
+birthdate_entry.grid(column=1, row=7)
+favorite_location_entry.grid(column=1, row=8)
+phone_number_entry.grid(column=1, row=9)
+
+button = Button(window, command = chef_table_fill_out, text="OK").grid(column = 0, row = 10)
+
+
+window.mainloop()
+
+
+#chef_table_fill_out()
 
 # how to get a screenshot
 # driver.get_screenshot_as_file("ChefTableForm.png")
-
-# setting a wait time
-driver.implicitly_wait(30)
-
-# get elements
-f_name = driver.find_element_by_id('First_Nm').send_keys('Julian')
-l_name = driver.find_element_by_id('Last_Nm').send_keys('Javier')
-email_address = driver.find_element_by_id('Email_Address').send_keys('JulianJavier@gmail.com')
-email_address_confirm = driver.find_element_by_id('Cnfm_Email_Address').send_keys('JulianJavier@gmail.com')
-address_line_1 = driver.find_element_by_id('AddressLine1').send_keys('3261 SW 160th Ave')
-address_line_2 = driver.find_element_by_id('AddressLine2').send_keys('')
-city = driver.find_element_by_id('City').send_keys(' Miramar')
-state = Select(driver.find_element_by_id('State')).select_by_value("FL")
-zip_code = driver.find_element_by_id('Zip').send_keys('33027')
-birthdate_month = Select(driver.find_element_by_id('bdaymonth')).select_by_value("07")
-birthdate_day = Select(driver.find_element_by_id('bdayday')).select_by_value("7")
-birthdate_year = Select(driver.find_element_by_id('bdayyear')).select_by_value("1990")
-favorite_location = Select(driver.find_element_by_id('store_code')).select_by_value("1053")
-phone_number_1 = driver.find_element_by_id('phonenum-1').send_keys('954')
-phone_number_2 = driver.find_element_by_id('phonenum-2').send_keys('663')
-phone_number_3 = driver.find_element_by_id('phonenum-3').send_keys('2294')
