ca0f52d82ac4a10804c070114d5051a0d56d8737
Jesus Vita <jesusvitaa@gmail.com>
2018-06-20 20:41:42 -0400

    modified to work with another site

--- chefTable.py
+++ chefTable.py
@@ -13,16 +13,8 @@ import pyautogui
 
 
 # Functions
-def get_emails():
-    email_list = open("emails.txt").read()
-    emailRegex = re.compile(r'''(
-        [a-zA-Z0-9._%+-] +              # username
-        @                               # a symbol
-        [a-zA-Z0-9.-]  +               # domain name
-        \.[a-zA-Z]{2,4}  # dot-somthing
-        )''', re.X)
-
-    return emailRegex.findall(email_list)
+def get_email(name):
+    return name.group(2) + name.group(1) + str(n.randint(1, 999)) + "@gmail.com"
 
 def get_random_name():
     name_list = open("names.txt").read().splitlines()
@@ -30,13 +22,11 @@ def get_random_name():
     name_regex = re.compile(r'(\w*),\s(\w*)')
     return name_regex.search(random_line)
 
-def get_random_address():
-    house_number = str(n.randint(100, 9999))
-    directions = ['E', 'N', 'W', 'S', 'SE','NE','SW','NW']
-    street_number = str(n.randint(1, 190))
-    street_type = ['AVE', 'BLVD', 'CT', 'PL', 'ST', 'TER', 'LN']
-
-    return house_number + " " + random.choice(directions) + " " + street_number + " " + random.choice(street_type)
+def random_phone_number():
+        phone_number_1 = random.choice(area_codes)
+        phone_number_2 = str(n.randint(100, 999))
+        phone_number_3 = str(n.randint(1000, 9999))
+        return phone_number_1 + phone_number_2 + phone_number_3
 
 def get_driver(x):
     if x == 'w':
@@ -44,7 +34,7 @@ def get_driver(x):
     else:
         return webdriver.Chrome(r"/Users/jesus/Desktop/PythonPractice/driver/chromedriver 2")
 
-def chef_table_fill_out(email):
+def chef_table_fill_out():
 
     # The website has some kind of detection to prevent fasle account maybe even prevent botting
     # Luckily with this I can trick it to letting me make an account 100%
@@ -67,7 +57,7 @@ def chef_table_fill_out(email):
     # timeout if not loaded
     driver.set_page_load_timeout(30)
 
-    driver.get("http://profile.benihana.com/registration/")
+    driver.get("https://acfp.myguestaccount.com/guest/enroll?card-template=ktYylJhNj2k%3d")
 
     # setting a wait time before inputting to make sure everything is loaded
     driver.implicitly_wait(30)
@@ -76,47 +66,42 @@ def chef_table_fill_out(email):
     name = get_random_name()
 
     # Birthdate
-    time = datetime.datetime.now()
-    bmonth = str(time.month)
-    bday = str(time.day + 1)
+    #time = datetime.datetime.now()
+    #bmonth = str(time.month)
+    #bday = str(time.day + 1)
 
-    # Random address
-    address = get_random_address()
+    # Name information
+    driver.find_element_by_xpath("//*[@id='firstName']").send_keys(str(name.group(2)))
+    driver.find_element_by_xpath("//*[@id='lastName']").send_keys(str(name.group(1)))
 
-    f_name = driver.find_element_by_id('First_Nm').send_keys(str(name.group(2)))
-    l_name = driver.find_element_by_id('Last_Nm').send_keys(str(name.group(1)))
-    email_address = driver.find_element_by_id('Email_Address').send_keys(str(email))
-    email_address_confirm = driver.find_element_by_id('Cnfm_Email_Address').send_keys(str(email))
+    # Favorite Store
+    Select(driver.find_element_by_xpath("//*[@id='favoriteStoreState']")).select_by_value("110")
+    time.sleep(5)
+    Select(driver.find_element_by_xpath("//*[@id='favoriteStore']")).select_by_value(random.choice(Stores))
 
-    # Address
-    address_line_1 = driver.find_element_by_id('AddressLine1').send_keys(address)
-    address_line_2 = driver.find_element_by_id('AddressLine2').send_keys('')
-    city = driver.find_element_by_id('City').send_keys('Miami')
-    state = Select(driver.find_element_by_id('State')).select_by_value('FL')
-    zip_code = driver.find_element_by_id('Zip').send_keys(str(n.randint(33101, 33299)))
+    # Zip code
+    driver.find_element_by_xpath("//*[@id='postalCode']").send_keys(str(n.randint(33101, 33299)))
+
+    # Phone number
+    driver.find_element_by_xpath("//*[@id='mobilePhone']").send_keys(random_phone_number())
 
     # Birthdate
-    birthdate_month = Select(driver.find_element_by_id('bdaymonth')).select_by_value("0" + bmonth)
-    birthdate_day = Select(driver.find_element_by_id('bdayday')).select_by_value(bday)
-    birthdate_year = Select(driver.find_element_by_id('bdayyear')).select_by_value(str(n.randint(1979, 1999)))
+    driver.find_element_by_xpath("//*[@id='dateOfBirthMonth']").send_keys("01")
+    driver.find_element_by_xpath("//*[@id='dateOfBirthDay']").send_keys("12")
+    driver.find_element_by_xpath("//*[@id='dateOfBirthYear']").send_keys("1995")
 
-    # always set to coral springs
-    favorite_location = Select(driver.find_element_by_id('store_code')).select_by_value("1065")
+    # Email
+    driver.find_element_by_xpath("//*[@id='email']").send_keys(get_email(name))
 
-    # Survey
-    driver.find_element_by_xpath("//*[@id='chefRegi']/fieldset[2]/div/div[1]/label[3]/label/span").click()
-    driver.find_element_by_xpath("//*[@id='chefRegi']/fieldset[2]/div/div[2]/span[2]/label/label/span").click()
-    Select(driver.find_element_by_id('visitFreq')).select_by_value("1M")
-    driver.find_element_by_xpath("//*[@id='favoriteDishesSortableList']/li[1]/label/label/span").click()
-    driver.find_element_by_xpath("//*[@id='chefRegi']/fieldset[2]/div/div[5]/label[2]/label/span").click()
-    driver.find_element_by_xpath("//*[@id='chefRegi']/fieldset[2]/div/div[6]/span[3]/label/label/span").click()
-    driver.find_element_by_xpath("//*[@id='chefRegi']/fieldset[2]/div/div[7]/label[2]/label/span").click()
+    # Password
+    driver.find_element_by_xpath("//*[@id='password']").send_keys(name.group(2) + "123!")
+    driver.find_element_by_xpath("//*[@id='confirmPassword']").send_keys(name.group(2) + "123!")
 
     # Anti-detection
-    backspace()
+    # backspace()
 
     # Submit
-    driver.find_element_by_xpath("//*[@id='submit-btn']").click()
+    # driver.find_element_by_xpath("//*[@id='submit-btn']").click()
 
 def run():
     emails = get_emails()
@@ -126,6 +111,8 @@ def run():
 States = ["AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA","MD","ME","MH","MI"
 ,"MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA"
 ,"VI","VT","WA","WI","WV","WY"]
+Stores = ["1012", "1061", "1018", "1030", "1025", "1047", "1015", "1017", "1026", "1066", "1010", "1034", "1029", "1065", "1064"]
+area_codes = ["786", "305", "954", "754"]
 n = random.SystemRandom()
 
 # GUI
@@ -133,7 +120,7 @@ window = tk.Tk()
 window.title("Chef's Table Filler")
 window.geometry("400x400")
 
-button = Button(window, command = run, text="OK", width = 10, height = 4).grid(row=5, column=0)
+button = Button(window, command = chef_table_fill_out, text="OK", width = 10, height = 4).grid(row=5, column=0)
 
 
 
